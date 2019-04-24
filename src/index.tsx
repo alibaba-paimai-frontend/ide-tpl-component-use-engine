1374827938f5dc4d2f1f353d71cfc305a24313a0
bocheng.zbc <bocheng.zbc@alibaba-inc.com>
2019-04-24 17:02:27 +0800

    使用initSuitsFromConfig

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,5 @@
 import { Instance } from 'mobx-state-tree';
-import { initSuits } from 'ide-lib-engine';
+import { initSuitsFromConfig } from 'ide-lib-engine';
 
 export * from './[CLASSNAME]/config';
 export * from './[CLASSNAME]/';
@@ -7,37 +7,13 @@ export * from './[CLASSNAME]/';
 import { [CLASSNAME]Currying } from './[CLASSNAME]/';
 import { config[CLASSNAME] } from './[CLASSNAME]/config';
 
-// 抽离子组件配置项
-const subStoresModelMap = {} as any;
-const subFactoryMap = {} as any;
-[SUBCOMP_START]
-const subComponents = config[CLASSNAME].component.children as any;
-const subComponentNames = Object.keys(subComponents);
-subComponentNames.forEach((name: string) => {
-    subStoresModelMap[name] = subComponents[name].storesModel;
-    subFactoryMap[name] = subComponents[name].factory;
-});
-[SUBCOMP_END]
-
 const {
     ComponentModel: [CLASSNAME]Model,
     NormalComponent: [CLASSNAME],
     ComponentHOC: [CLASSNAME]HOC,
     ComponentAddStore: [CLASSNAME]AddStore,
     ComponentFactory: [CLASSNAME]Factory
-} = initSuits({
-    ComponentCurrying: [CLASSNAME]Currying,
-    className: config[CLASSNAME].component.className,
-    solution: config[CLASSNAME].component.solution,
-    defaultProps: config[CLASSNAME].component.defaultProps,
-    controlledKeys: config[CLASSNAME].model.controlledKeys,
-    modelProps: config[CLASSNAME].model.props,
-    subComponents: config[CLASSNAME].component.children,
-    subStoresModelMap: subStoresModelMap,
-    subFactoryMap: subFactoryMap,
-    idPrefix: config[CLASSNAME].store.idPrefix,
-    routerConfig: config[CLASSNAME].router
-});
+} = initSuitsFromConfig([CLASSNAME]Currying,config[CLASSNAME]);
 
 export {
     [CLASSNAME]Model,
